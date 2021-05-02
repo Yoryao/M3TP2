7f717405414ed291f4e9dd16827eb0ba0c8323ff
yoryao <jorgejoaquinrivera@gmail.com>
2021-05-02 01:11:35 -0300

    55

--- server.js
+++ server.js
@@ -1,7 +1,38 @@
-var http = require('http');
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded());
+
+app.get('/myDataBase', function(req, res) {
+
+    var name = req.body.nombre;
+    res.send(name);
+
+});
+
+
+
+app.listen(8080, function() {
+
+    console.log('esto no va a funcionar');
+
+
+});
+
+
+
+
+
+
+
+
+/* var http = require('http');
+var url = require('url');
 
 //create a server object:
 http.createServer(function(req, res) {
     res.write('Hello and now part 3'); //write a response to the client
+    res.write('seguimos');
     res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
+}).listen(8080); //the server object listens on port 8080 */
